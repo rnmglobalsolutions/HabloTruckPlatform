1833eebda1d0f3a431f13421188c43f0f6d5e187
Raisel <devnraisel@outlook.com>
2026-03-09 22:28:55 -0500

    Table name change

--- HabloTruckPlatform.Infrastructure/Storage/Entities/TableNames.cs
+++ HabloTruckPlatform.Infrastructure/Storage/Entities/TableNames.cs
@@ -2,18 +2,18 @@
 
 public static class TableNames
 {
-    public const string Users = "HT_Users";
-    public const string UserEmail = "HT_UserEmail";
-    public const string UserManyChat = "HT_UserManyChat";
-    public const string UserStripeCustomer = "HT_UserStripeCustomer";
-    public const string GraceIndex = "HT_GraceIndex";
-    public const string StripeEvents = "HT_StripeEvents";
-    public const string Companies = "HT_Companies";
-    public const string Entitlements = "HT_Entitlements";
-    public const string EntitlementExpiryIndex = "HT_EntitlementExpiryIndex";
-    public const string Seats = "HT_Seats";
-    public const string InviteCodes = "HT_InviteCodes";
-    public const string InviteCompanyIndex = "HT_InviteCompanyIndex";
-    public const string FailedActions = "HT_FailedActions";
-    public const string StripeEventAudit = "HT_StripeEventAudit";
+    public const string Users = "HTUsers";
+    public const string UserEmail = "HTUserEmail";
+    public const string UserManyChat = "HTUserManyChat";
+    public const string UserStripeCustomer = "HTUserStripeCustomer";
+    public const string GraceIndex = "HTGraceIndex";
+    public const string StripeEvents = "HTStripeEvents";
+    public const string Companies = "HTCompanies";
+    public const string Entitlements = "HTEntitlements";
+    public const string EntitlementExpiryIndex = "HTEntitlementExpiryIndex";
+    public const string Seats = "HTSeats";
+    public const string InviteCodes = "HTInviteCodes";
+    public const string InviteCompanyIndex = "HTInviteCompanyIndex";
+    public const string FailedActions = "HTFailedActions";
+    public const string StripeEventAudit = "HTStripeEventAudit";
 }
